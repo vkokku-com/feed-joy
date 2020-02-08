2cf012899f9159a74f2ce457335ed879fc9db133
Gupta <ashish.gupta@epicor.com>
2020-02-08 11:50:23 +0530

    Revert "vkokku:updated server"
    This reverts commit 7fa4836f64ef1ee719f6c58a0acde4447299e25d.

--- app.js
+++ app.js
@@ -13,44 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Simple Express Site!' });
-});
-app.get('/home', (req, res) => {
-    res.render('index', { title: 'Simple Express Site!' });
-});
-app.get('/about', (req, res) => {
-    res.render('about');
-});
-app.get('/contact', (req, res) => {
-    res.render('contact');
-});
-app.post('/contact/send', (req, res) => {
-    var transporter = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-            user: 'your mail id',
-            pass: 'your password'
-        }
-    });
-
-    var mailOptions = {
-        from: 'senders mail id',
-        to: 'recepients mail id',
-        subject: 'nodemailer Test mail',
-        text: 'You have submitted a form with the following details:' + '\n' + 'Name' + '\t:' +
-            req.body.name + '\n' + 'Email' + '\t:' + req.body.email + '\n' + 'Message' + '\t:' + req.body.message,
-        html: '<p>You have submitted a form with the following details:</p><ul><li>Name:' + req.body.name + '</li><li>Email:' + req.body.email + '</li><li>Message:' + req.body.message + '</li></ul>'
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log(error);
-            res.redirect('/');
-        } else {
-            console.log('Message Sent:', info.response);
-            res.redirect('/');
-        }
-    });
+    res.render('index');
 });
 
 app.listen(3000);
