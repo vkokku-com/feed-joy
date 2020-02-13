462ddc710856f0970d49eda4d81626813f0d7d09
Kokku <venkata.kokku@epicor.com>
2020-02-14 00:40:55 +0530

    client code committed

---
+++ client/App.js
@@ -0,0 +1,228 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Image,
+  ImageBackground,
+  TextInput,
+  TouchableOpacity
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const background = require("./background.jpg");
+const lockIcon = require("./lock.png");
+const personIcon = require("./person.png");
+
+class App extends Component {
+  render() {
+    return (
+      <ImageBackground 
+        style={[styles.background, styles.container]} 
+        source={background}
+        resizeMode="cover"
+      >
+        <View style={styles.container} />
+        <View style={styles.wrapper}>
+          <View style={styles.inputWrap}>
+            <View style={styles.iconWrap}>
+              <Image
+                source={personIcon}
+                style={styles.icon}
+                resizeMode="contain"
+              />
+            </View>
+            <TextInput
+              placeholder="Username"
+              style={styles.input}
+              underlineColorAndroid="transparent"
+            />
+          </View>
+          <View style={styles.inputWrap}>
+            <View style={styles.iconWrap}>
+              <Image
+                source={lockIcon}
+                style={styles.icon}
+                resizeMode="contain"
+              />
+            </View>
+            <TextInput
+              placeholder="Password"
+              secureTextEntry
+              style={styles.input}
+              underlineColorAndroid="transparent"
+            />
+          </View>
+          <TouchableOpacity activeOpacity={.5}>
+            <View style={styles.button}>
+              <Text style={styles.buttonText}>Sign In</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={.5}>
+            <View>
+              <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.container} />
+      </ImageBackground>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  background: {
+    width: null,
+    height: null
+  },
+  wrapper: {
+    paddingHorizontal: 15,
+  },
+  inputWrap: {
+    flexDirection: "row",
+    marginVertical: 10,
+    height: 40,
+    backgroundColor: "transparent"
+  },
+  input: {
+    flex: 1,
+    paddingHorizontal: 10,
+    backgroundColor: '#FFF'
+  },
+  iconWrap: {
+    paddingHorizontal: 7,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#d73352"
+  },
+  icon: {
+    width: 20,
+    height: 20,
+  },
+  button: {
+    backgroundColor: "#d73352",
+    paddingVertical: 15,
+    marginVertical: 15,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  buttonText: {
+    color: "#FFF",
+    fontSize: 18
+  },
+  forgotPasswordText: {
+    color: "#FFF",
+    backgroundColor: "transparent",
+    textAlign: "center"
+  }
+});
+
+// const App: () => React$Node = () => {
+//   return (
+//     <>
+//       <StatusBar barStyle="dark-content" />
+//       <SafeAreaView>
+//         <ScrollView
+//           contentInsetAdjustmentBehavior="automatic"
+//           style={styles.scrollView}>
+//           <Header />
+//           {global.HermesInternal == null ? null : (
+//             <View style={styles.engine}>
+//               <Text style={styles.footer}>Engine: Hermes</Text>
+//             </View>
+//           )}
+//           <View style={styles.body}>
+//             <View style={styles.sectionContainer}>
+//               <Text style={styles.sectionTitle}>Step One</Text>
+//               <Text style={styles.sectionDescription}>
+//                 Edit <Text style={styles.highlight}>App.js</Text> to change this
+//                 screen and then come back to see your edits.
+//               </Text>
+//             </View>
+//             <View style={styles.sectionContainer}>
+//               <Text style={styles.sectionTitle}>See Your Changes</Text>
+//               <Text style={styles.sectionDescription}>
+//                 <ReloadInstructions />
+//               </Text>
+//             </View>
+//             <View style={styles.sectionContainer}>
+//               <Text style={styles.sectionTitle}>Debug</Text>
+//               <Text style={styles.sectionDescription}>
+//                 <DebugInstructions />
+//               </Text>
+//             </View>
+//             <View style={styles.sectionContainer}>
+//               <Text style={styles.sectionTitle}>Learn More</Text>
+//               <Text style={styles.sectionDescription}>
+//                 Read the docs to discover what to do next:
+//               </Text>
+//             </View>
+//             <LearnMoreLinks />
+//           </View>
+//         </ScrollView>
+//       </SafeAreaView>
+//     </>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   scrollView: {
+//     backgroundColor: Colors.lighter,
+//   },
+//   engine: {
+//     position: 'absolute',
+//     right: 0,
+//   },
+//   body: {
+//     backgroundColor: Colors.white,
+//   },
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24,
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: '600',
+//     color: Colors.black,
+//   },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: '400',
+//     color: Colors.dark,
+//   },
+//   highlight: {
+//     fontWeight: '700',
+//   },
+//   footer: {
+//     color: Colors.dark,
+//     fontSize: 12,
+//     fontWeight: '600',
+//     padding: 4,
+//     paddingRight: 12,
+//     textAlign: 'right',
+//   },
+// });
+
+export default App;
